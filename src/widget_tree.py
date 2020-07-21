994244fab15271ba7f37d8ae5ccb89a24fb8be2c
fkubota <rkkubotay@gmail.com>
2020-07-21 10:05:40 +0900

    [Update] widget_tree を実装

--- src/widget_tree.py
+++ src/widget_tree.py
@@ -5,40 +5,31 @@ import PyQt5.QtWidgets as QW
 class WidgetTree(QW.QWidget):
     def __init__(self):
         super().__init__()
-        self.pbar = QW.QProgressBar()
-        self.lbl = QW.QLabel()
 
-        self.init_ui()
-        self.init_method()
+        self.model = QW.QFileSystemModel()
+        self.tree = QW.QTreeView()
 
     def init_ui(self):
-        self.resize(300, 80)
-        self.move(500, 500)
-
-        # layout
-        vbox0 = QW.QVBoxLayout()
-        vbox0.addWidget(self.lbl)
-        vbox0.addWidget(self.pbar)
-        self.setLayout(vbox0)
+        self.tree.resize(640, 480)
+        vhox0 = QW.QVBoxLayout()
+        vhox0.addWidget(self.tree)
+        self.setLayout(vhox0)
 
     def init_method(self):
-        self.lbl.setText('no message')
+        self.model.setRootPath('/')
+        self.tree.setModel(self.model)
+        self.tree.setAnimated(False)
+        self.tree.setIndentation(20)
+        self.tree.setSortingEnabled(True)
 
 
 def main():
     app = QW.QApplication(sys.argv)
 
-    w = WidgetProgressBar()
+    w = WidgetTree()
+    w.tree.setRootIndex(w.model.index("../data/"))
     w.move(600, 500)
-    w.pbar.setMaximum(100)
-    w.pbar.setValue(30)
     w.show()
-
-    import time
-    for i in range(1, 101):
-        QW.QApplication.processEvents()
-        w.pbar.setValue(i)
-        time.sleep(0.05)
 
     sys.exit(app.exec_())
 
